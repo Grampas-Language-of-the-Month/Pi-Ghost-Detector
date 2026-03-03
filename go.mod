dc3d7036b66cd47434c1d0a6dce3c537b86ea2a3
Josh Walker <walkerje1992@gmail.com>
2026-03-02 19:03:50 -0700

    Add HC-SR501 module

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.25.0
 require github.com/fatih/color v1.18.0
 
 require (
+	github.com/Low-Level-Devils/HC-SR501-Module_GoLang v0.0.0-20260303015200-b94527c11668 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	periph.io/x/conn/v3 v3.7.2 // indirect
+	periph.io/x/host/v3 v3.8.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	sr501_mod "github.com/Low-Level-Devils/HC-SR501-Module_GoLang"
 	"github.com/fatih/color"
 )
 
 func main() {
 	color.Cyan("Starting...")
+
+	pirSensor, err := sr501_mod.NewSensor("GPIO17")
+
+	if err != nil {
+		color.Red("Failed to start sensor")
+
+		return
+	}
+
+	go pirSensor.Watch()
+
+	color.Blue("Sensor initialized")
+
+	for pirEvent := range pirSensor.Events {
+		if pirEvent {
+			color.Green("Motion Detected")
+		} else {
+			color.Green("Sensor reset")
+		}
+	}
 }
